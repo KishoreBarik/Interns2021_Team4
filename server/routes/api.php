a477aeb098b442fbb37c494979f946c81cd1f2e8
mouli <mouliballanki143@gmail.com>
2022-02-05 21:18:17 +0530

    installed passport and done some configurations

--- server/routes/api.php
+++ server/routes/api.php
@@ -143,18 +143,18 @@ Route::group(['prefix' => 'departments'], function () {
     Route::delete('/{id}', [DepartmentController::class, 'destroy'])->name('api.departments.delete');
 });
 
-Route::group(['prefix' => 'clients'], function () {
-    Route::get('/', [ClientController::class, 'index'])->name('api.clients.index');
-    Route::post('/', [ClientController::class, 'create'])->name('api.clients.create');
-    Route::get('/{id}', [ClientController::class, 'show'])->name('api.clients.show');
-    Route::put('/{id}', [ClientController::class, 'update'])->name('api.clients.update');
-    Route::delete('/{id}', [ClientController::class, 'destroy'])->name('api.clients.destroy');
-});
-//CRUD routes for projects
-Route::group(['prefix' => 'projects'], function () {
-    Route::get('/', [ProjectController::class, 'index'])->name('api.projects.index');
-    Route::post('/', [ProjectController::class, 'create'])->name('api.projects.create');
-    Route::get('/{id}', [ProjectController::class, 'show'])->name('api.projects.show');
-    Route::put('/{id}', [ProjectController::class, 'update'])->name('api.projects.update');
-    Route::delete('/{id}', [ProjectController::class, 'destroy'])->name('api.projects.destroy');
-});
+// Route::group(['prefix' => 'clients'], function () {
+//     Route::get('/', [ClientController::class, 'index'])->name('api.clients.index');
+//     Route::post('/', [ClientController::class, 'create'])->name('api.clients.create');
+//     Route::get('/{id}', [ClientController::class, 'show'])->name('api.clients.show');
+//     Route::put('/{id}', [ClientController::class, 'update'])->name('api.clients.update');
+//     Route::delete('/{id}', [ClientController::class, 'destroy'])->name('api.clients.destroy');
+// });
+// //CRUD routes for projects
+// Route::group(['prefix' => 'projects'], function () {
+//     Route::get('/', [ProjectController::class, 'index'])->name('api.projects.index');
+//     Route::post('/', [ProjectController::class, 'create'])->name('api.projects.create');
+//     Route::get('/{id}', [ProjectController::class, 'show'])->name('api.projects.show');
+//     Route::put('/{id}', [ProjectController::class, 'update'])->name('api.projects.update');
+//     Route::delete('/{id}', [ProjectController::class, 'destroy'])->name('api.projects.destroy');
+// });
